a66874964dc6ed51575e1f051c2827e8030227c8
Sai Sreekar <sai.sreekhar@gmail.com>
2024-08-06 10:46:00 +0530

    .env added

---
+++ backend/db/dbConfig.js
@@ -0,0 +1,10 @@
+require("dotenv").config(); // Load environment variables from .env file
+
+const dbConfig = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+};
+
+module.exports = dbConfig;
--- backend/db/dbConnection.js
+++ backend/db/dbConnection.js
@@ -1,18 +1,14 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
+const dbConfig = require("./dbConfig");
 
-const connection = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'inshorts_user',
-    password: '1234QWERqwer#$',
-    database: 'inshorts'
-});
+const connection = mysql.createConnection(dbConfig);
 
 connection.connect((err) => {
-    if (err) {
-        console.error('Error connecting to the database:', err);
-        return;
-    }
-    console.log('Connected to the MySQL database.');
+  if (err) {
+    console.error("Error connecting to the database:", err);
+    return;
+  }
+  console.log("Connected to the MySQL database.");
 });
 
 module.exports = connection;
